6a96ad119eb11c4300a89aa1d7904823a10dde81
poweredbyskx <poweredbyskx@gmail.com>
2025-05-18 17:54:10 +0500

    исправлен текст

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,18 +1,19 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm as DjangoUserCreationForm
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserChangeForm
 
 User = get_user_model()
 
 class CustomUserCreationForm(DjangoUserCreationForm):
     class Meta(DjangoUserCreationForm.Meta):
         model = User
-        fields = ['first_name', 'last_name', 'username',
-                  'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
 
-
-
-class CustomUserChangeForm(UserChangeForm):
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'username']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Добавляем плейсхолдер для username
+        self.fields['username'].widget.attrs.update({'placeholder': 'Имя пользователя'})
+        self.fields['first_name'].widget.attrs.update({'placeholder': 'Имя'})
+        self.fields['last_name'].widget.attrs.update({'placeholder': 'Фамилия'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Пароль'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Подтверждение пароля'})
