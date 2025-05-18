13ed1caf4e402a400d98b42a34c381811cf0d5c8
poweredbyskx <poweredbyskx@gmail.com>
2025-05-18 18:46:59 +0500

    исправлен текст

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,7 +1,27 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    UserChangeForm,
+    AuthenticationForm,
+)
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class CustomUserCreationForm(UserCreationForm):
+    """Form for creating new users"""
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "email")
+
+class CustomUserChangeForm(UserChangeForm):
+    """Form for updating existing users"""
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "email")
 
 class CustomAuthForm(AuthenticationForm):
+    """Authentication form with Russian placeholders"""
     username = forms.CharField(
         label="Имя пользователя",
         widget=forms.TextInput(attrs={
@@ -12,6 +32,7 @@ class CustomAuthForm(AuthenticationForm):
     )
     password = forms.CharField(
         label="Пароль",
+        strip=False,
         widget=forms.PasswordInput(attrs={
             "placeholder": "Пароль",
             "class": "form-control",
