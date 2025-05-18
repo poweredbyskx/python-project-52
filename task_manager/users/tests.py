70f9c9f9dddae7c0265e0fe657cba0f13ec92287
poweredbyskx <poweredbyskx@gmail.com>
2025-05-19 01:28:06 +0500

    работа над ошибками

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,6 +1,6 @@
+import datetime
 from django.test import TestCase, Client
 from task_manager.users.models import User
-
 
 class UserCRUDTest(TestCase):
     fixtures = ['users.json']
@@ -20,4 +20,10 @@ class UserCRUDTest(TestCase):
         self.assertEqual(response.status_code, 302)  # обычно редирект после создания
         self.assertTrue(User.objects.filter(username='john').exists())
         user = User.objects.get(username='john')
+
+        # Проверка, что дата создания - datetime
         self.assertIsInstance(user.date_joined, datetime.datetime)
+
+        # Проверка правильности пароля
+        self.assertTrue(user.check_password('testpass123'))
+tetime)
