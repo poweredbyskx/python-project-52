b51af55010cf257d09e882270c396026ade347af
poweredbyskx <poweredbyskx@gmail.com>
2025-05-20 21:41:50 +0500

    работа над ошибками

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.utils.translation import gettext
+from django.urls import reverse
 
 
 class FormView:
@@ -18,6 +19,9 @@ class DeleteView:
 
 
 class EditView:
+    def get_success_url(self):
+        return reverse(self.success_url_name)
+
     def post(self, request, *args, **kwargs):
         value_id = kwargs.get('pk')
         value = self.value.objects.get(id=value_id)
@@ -26,6 +30,6 @@ class EditView:
             form.save()
             value_edit = gettext(self.text)
             messages.add_message(request, messages.SUCCESS, value_edit)
-            return redirect(self.path)
+            return redirect(self.get_success_url())
         return render(request, self.template,
                       {'form': form, 'value_id': value_id})
