eb5d9c9f9945f251e53353870d6756d3fcf33841
poweredbyskx <poweredbyskx@gmail.com>
2025-05-30 00:20:15 +0500

    финальные правки

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     path('', TasksView.as_view(), name='list'),
     path('create/', TaskFormCreateView.as_view(), name='create'),
     path('<int:pk>/update/', TaskFormEditView.as_view(), name='edit_task'),
-    path('<int:pk>/delete/', TaskFormDeleteView.as_view(),
-         name='drop_task'),
+    path('<int:pk>/delete/', TaskFormDeleteView.as_view(), name='drop_task'),
     path('<int:pk>/', TaskFormView.as_view(), name='view_task'),
 ]
